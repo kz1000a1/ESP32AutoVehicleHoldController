99c6f9a923d8b36be89e28220a87bc8d94a1481b
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-04-23 22:30:34 +0900

    Update ESP32AutoVehicleHoldController.ino

--- ESP32AutoVehicleHoldController.ino
+++ ESP32AutoVehicleHoldController.ino
@@ -119,28 +119,28 @@ bool if_can_message_receive_is_pendig() {
 
   // Check if alert happened
   twai_read_alerts(&alerts_triggered, pdMS_TO_TICKS(POLLING_RATE_MS));
-  twai_get_status_info(&twaistatus);
 
-  // Handle alerts
-  if (alerts_triggered & TWAI_ALERT_ERR_PASS) {
-    if (DebugMode == DEBUG) {
+  if (DebugMode == DEBUG) {
+  　twai_get_status_info(&twaistatus);
+
+  　// Handle alerts
+  　if (alerts_triggered & TWAI_ALERT_ERR_PASS) {
       Serial.println("# Alert: TWAI controller has become error passive.");
     }
-  }
-  if (alerts_triggered & TWAI_ALERT_BUS_ERROR) {
-    if (DebugMode == DEBUG) {
+    
+    if (alerts_triggered & TWAI_ALERT_BUS_ERROR) {
       Serial.println("# Alert: A (Bit, Stuff, CRC, Form, ACK) error has occurred on the bus.");
       Serial.printf("# Bus error count: %d\n", twaistatus.bus_error_count);
     }
-  }
-  if (alerts_triggered & TWAI_ALERT_RX_QUEUE_FULL) {
-    if (DebugMode == DEBUG) {
+    
+    if (alerts_triggered & TWAI_ALERT_RX_QUEUE_FULL) {
       Serial.println("# Alert: The RX queue is full causing a received frame to be lost.");
       Serial.printf("# RX buffered: %d\t", twaistatus.msgs_to_rx);
       Serial.printf("RX missed: %d\t", twaistatus.rx_missed_count);
       Serial.printf("RX overrun %d\n", twaistatus.rx_overrun_count);
     }
   }
+  
   // If CAN message receive is pending, process the message
   if (alerts_triggered & TWAI_ALERT_RX_DATA) {
     return true;
